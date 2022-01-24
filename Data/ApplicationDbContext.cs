ecffe579d3f62fbb95db44f492e5caec280f8d2b
Joshua Tingler <jtingler96@gmail.com>
2022-01-24 10:37:15 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using myBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myBlog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BlogUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+
+    }
+}
